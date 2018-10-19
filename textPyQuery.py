d84b97bcb3eeba74524b0d1def7df820cb9ec705
xiaozongzi <478359031@qq.com>
2018-10-19 17:23:02 +0800

    qq

--- textPyQuery.py
+++ textPyQuery.py
@@ -13,16 +13,24 @@ htmlcont = htmlf.read()
 # print(htmlcont)
 doc = pq(htmlcont)
 # print(doc)
-section = doc('section')
+section = doc('section')('[data-id=\'2660\']')
 # print(section)
 for item in section.items():
-        print(item('[data-id=\'2660\']'))
+    name = item('p')(
+        '[style="padding-right: 20px;padding-left: 20px;font-size: 16px;color: inherit;border-color: rgb(166, 91, 203);"]').text()
+    p = item('span')('[style="font-size: 15px;color: rgb(255, 104, 39);"]')
+    print(name)
+    for span in p.items():
+        s = span.text()
+        print(s)
+        if s.__contains__('：'):
+            print(s[s.index('：')+1:])
 
-    # if item.attr('data-id') == '2660':
-    #     print(item)
-    #     span = item('[style=font-size: 15px;color: rgb(255, 104, 39)]')
-    #     for spanItem in span.items():
-    #         if spanItem.attr("style") == 'font-size: 15px;color: rgb(255, 104, 39);':
-    #             print(span)
+# if item.attr('data-id') == '2660':
+#     print(item)
+#     span = item('[style=font-size: 15px;color: rgb(255, 104, 39)]')
+#     for spanItem in span.items():
+#         if spanItem.attr("style") == 'font-size: 15px;color: rgb(255, 104, 39);':
+#             print(span)
 
 # doc
