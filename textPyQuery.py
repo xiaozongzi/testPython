1d44d8b8334ce1dbe33baa5a0079e438f35605ca
xiaozongzi <478359031@qq.com>
2018-10-23 17:31:41 +0800

    qq

--- textPyQuery.py
+++ textPyQuery.py
@@ -1,4 +1,5 @@
 from pyquery import PyQuery as pq
+import pandas as pd
 
 # import urllib3
 # page = urllib3.("http://example")
@@ -14,17 +15,42 @@ htmlcont = htmlf.read()
 doc = pq(htmlcont)
 # print(doc)
 section = doc('section')('[data-id=\'2660\']')
+
+
+def getData():
+    ss = s[s.index('：') + 1:]
+    if ss.__contains__("、"):
+        return ss.split("、")
+    elif ss.__contains__(" "):
+        return ss.split(" ")
+
+
 # print(section)
 for item in section.items():
     name = item('p')(
         '[style="padding-right: 20px;padding-left: 20px;font-size: 16px;color: inherit;border-color: rgb(166, 91, 203);"]').text()
     p = item('span')('[style="font-size: 15px;color: rgb(255, 104, 39);"]')
     print(name)
-    for span in p.items():
-        s = span.text()
-        print(s)
-        if s.__contains__('：'):
-            print(s[s.index('：')+1:])
+    first = p.eq(0)
+    s = first.text()
+
+    print(s)
+    if s.__contains__('：'):
+        print(s[s.index('：') + 1:])
+        data = getData()
+        print(data)
+        se = pd.Series(data)
+        print(se)
+    # for span in p.items():
+    #     s = span.text()
+    #     # s = span.text()
+    #     print(s)
+    # if s.__contains__('：'):
+    # print(s[s.index('：') + 1:])
+    # data = getData()
+    # print(data)
+    # se = pd.Series(data)
+    # print(se)
 
 # if item.attr('data-id') == '2660':
 #     print(item)
